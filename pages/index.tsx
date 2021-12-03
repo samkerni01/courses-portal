f801557f80c4638781f387a72309008153bd81da
samkerniol <pavelkorol3v@gmail.com>
2021-12-03 14:51:03 +0300

    Added a package axios and realization get data of API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { GetStaticProps } from 'next';
 import { useState } from 'react';
+import axios from 'axios';
 
 import { Title, Button, Text, Tag, Rating } from '../components';
 import { withLayout } from '../layout/Layout';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
     const [rating, setRating] = useState<number>(0);
 
     return (
@@ -25,8 +28,28 @@ function Home(): JSX.Element {
 
             <Rating rating={rating} setRating={setRating} />
             <Rating rating={3} />
+
+            <ul>
+                {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+            </ul>
         </>
     );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0;
+    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', { firstCategory });
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    };
+};
+
+interface HomeProps extends Record<string, unknown> {
+    menu: MenuItem[];
+    firstCategory: number;
+}
